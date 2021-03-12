e787a2c104f61be1d4fec0c01c38bb22208205cb
plalcedo <49098213+plalcedo@users.noreply.github.com>
2021-03-12 09:08:15 -0600

    Actualizando prácticas

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,13 +7,21 @@ function test() {
 }
 
 $("#verde").click(function() {
-    $("#color").addClass("bg-success");
+    $("#color").removeClass().addClass("bg-success");
+    $("#texto").text("Color:");
 });
 
 $("#rojo").click(function() {
-    $("#color").addClass("bg-danger");
+    $("#color").removeClass().addClass("bg-danger");
+    $("#texto").text("Color:");
 });
 
 $("#amarillo").click(function() {
-    $("#color").addClass("bg-warning");
+    $("#color").removeClass().addClass("bg-warning");
+    $("#texto").text("Color:");
+});
+
+$('#imagen').click(function() {
+    $("#color").removeClass().addClass("bgImg");
+    $("#texto").text("Imagen");
 });
